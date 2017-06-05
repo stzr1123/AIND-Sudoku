41d4d055575561951eca824ce201fc3c91084054
Esteban Zacharzewski <zacha.eg@gmail.com>
2017-06-05 23:08:19 +0200

    implemented diagonal sudoku

--- solution.py
+++ solution.py
@@ -16,6 +16,9 @@ boxes = cross(rows, cols)
 row_units = [cross(r, cols) for r in rows]
 column_units = [cross(rows, c) for c in cols]
 square_units = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')]
+diagonal_units_1 = [rs+cs for rs, cs in zip(rows, cols)]
+diagonal_units_2 = [rs+cs for rs, cs in zip(rows, reversed(cols))]
+diagonal_units = [diagonal_units_1, diagonal_units_2]
 unitlist = row_units + column_units + square_units
 units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
 peers = dict((s, set(sum(units[s],[]))-set([s])) for s in boxes)
@@ -217,6 +220,18 @@ def solve(grid):
     Returns:
         The dictionary representation of the final sudoku grid. False if no solution exists.
     """
+    global unitlist
+    global units
+    global peers
+
+    # update the units to include the two diagonals
+    unitlist = row_units + column_units + square_units + diagonal_units
+    units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
+    peers = dict((s, set(sum(units[s],[]))-set([s])) for s in boxes)
+
+    # run algorithm as usual
+    values = grid_values(grid)
+    return search(values)
 
 if __name__ == '__main__':
     diag_sudoku_grid = '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
