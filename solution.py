42666104e9ad9f669da20f956e3952f2ba634ff6
Esteban Zacharzewski <zacha.eg@gmail.com>
2017-06-05 16:59:30 +0200

    checking just columns, need to implement row checks as well

--- solution.py
+++ solution.py
@@ -1,3 +1,25 @@
+def cross(A, B):
+    """
+    NOTE: Code extracted from Udacity's Sudoku project lessons.
+
+    Cross product of elements in A and elements in B.
+    """
+    return [s+t for s in A for t in B]
+
+# Vars representing various aspects of a Sudoku board
+# NOTE: Code extracted from Udacity's Sudoku project lessons.
+rows = 'ABCDEFGHI'
+cols = '123456789'
+
+boxes = cross(rows, cols)
+
+row_units = [cross(r, cols) for r in rows]
+column_units = [cross(rows, c) for c in cols]
+square_units = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')]
+unitlist = row_units + column_units + square_units
+units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
+peers = dict((s, set(sum(units[s],[]))-set([s])) for s in boxes)
+
 assignments = []
 
 def assign_value(values, box, value):
@@ -15,7 +37,7 @@ def assign_value(values, box, value):
         assignments.append(values.copy())
     return values
 
-def naked_twins(values):
+def naked_twins(values: dict) -> dict:
     """Eliminate values using the naked twins strategy.
     Args:
         values(dict): a dictionary of the form {'box_name': '123456789', ...}
@@ -27,12 +49,61 @@ def naked_twins(values):
     # Find all instances of naked twins
     # Eliminate the naked twins as possibilities for their peers
 
-def cross(A, B):
-    "Cross product of elements in A and elements in B."
-    pass
+    # General solution:
+    # iterate over boxes, if it has two possibilities iterate over peers
+    # to see if any other have the same values, remove values from the other peers
+
+    # TODO: assert only one peer has the same 2 sols, otherwise don't eliminate values!
+    # apparently only works for the same column
+
+    solved_twin = set()
+
+    for box, partial_sols in values.items():
+        if len(partial_sols) == 2 and box not in solved_twin:
+            # box has two possible solutions
+            box_row = box[0]
+            box_col = box[1]
+
+            twin_1 = None
+            twin_2 = None
+
+            for peer_box in peers[box]:
+                peer_sols = values[peer_box]
+                if len(peer_sols) == 2:
+                    peer_box_row = peer_box[0]
+                    peer_box_col = peer_box[1]
+
+                    if (peer_box_col == box_col) or (peer_box_row == box_row):
+                        if all([peer_sols[0] in partial_sols, peer_sols[1] in partial_sols]):
+                            # peer box shares the same possible sols
+                            twin = peer_box
+                            solved_twin.add(twin)
+                            # print('{} {}'.format(box, twin))
+                            break
+
+            else:
+                # this box has no twin peer
+                # import ipdb; ipdb.set_trace()
+                continue
+
+            # iterate over peers again to remove the 2 possible sols
+            peers_subset = peers[box]
+            # list(map(peers_subset.add, peers[twin]))
+            for peer_box in peers_subset:
+                peer_box_row = peer_box[0]
+                peer_box_col = peer_box[1]
+                if peer_box == twin or peer_box_col != box_col:
+                    continue
+                for digit in partial_sols:
+                    values[peer_box] = values[peer_box].replace(digit, '')
+
+    return values
+
 
 def grid_values(grid):
     """
+    NOTE: Code extracted from Udacity's Sudoku project lessons.
+
     Convert grid into a dict of {square: char} with '123456789' for empties.
     Args:
         grid(string) - A grid in string form.
@@ -41,27 +112,105 @@ def grid_values(grid):
             Keys: The boxes, e.g., 'A1'
             Values: The value in each box, e.g., '8'. If the box has no value, then the value will be '123456789'.
     """
-    pass
+    chars = []
+    digits = '123456789'
+    for c in grid:
+        if c in digits:
+            chars.append(c)
+        if c == '.':
+            chars.append(digits)
+    assert len(chars) == 81
+    return dict(zip(boxes, chars))
 
 def display(values):
     """
+    NOTE: Code extracted from Udacity's Sudoku project lessons.
+
     Display the values as a 2-D grid.
     Args:
         values(dict): The sudoku in dictionary form
     """
-    pass
+    width = 1+max(len(values[s]) for s in boxes)
+    line = '+'.join(['-'*(width*3)]*3)
+    for r in rows:
+        print(''.join(values[r+c].center(width)+('|' if c in '36' else '')
+                      for c in cols))
+        if r in 'CF': print(line)
+    return
 
 def eliminate(values):
-    pass
+    """
+    NOTE: Code extracted from Udacity's Sudoku project lessons.
+
+    Go through all the boxes, and whenever there is a box with a value, eliminate this value from the values of all its peers.
+    Input: A sudoku in dictionary form.
+    Output: The resulting sudoku in dictionary form.
+    """
+    solved_values = [box for box in values.keys() if len(values[box]) == 1]
+    for box in solved_values:
+        digit = values[box]
+        for peer in peers[box]:
+            values[peer] = values[peer].replace(digit,'')
+    return values
 
 def only_choice(values):
-    pass
+    """
+    NOTE: Code extracted from Udacity's Sudoku project lessons.
+
+    Go through all the units, and whenever there is a unit with a value that only fits in one box, assign the value to this box.
+    Input: A sudoku in dictionary form.
+    Output: The resulting sudoku in dictionary form.
+    """
+    for unit in unitlist:
+        for digit in '123456789':
+            dplaces = [box for box in unit if digit in values[box]]
+            if len(dplaces) == 1:
+                values[dplaces[0]] = digit
+    return values
 
 def reduce_puzzle(values):
-    pass
+    """
+    NOTE: Code extracted from Udacity's Sudoku project lessons.
+
+    Iterate eliminate() and only_choice(). If at some point, there is a box with no available values, return False.
+    If the sudoku is solved, return the sudoku.
+    If after an iteration of both functions, the sudoku remains the same, return the sudoku.
+    Input: A sudoku in dictionary form.
+    Output: The resulting sudoku in dictionary form.
+    """
+    solved_values = [box for box in values.keys() if len(values[box]) == 1]
+    stalled = False
+    while not stalled:
+        solved_values_before = len([box for box in values.keys() if len(values[box]) == 1])
+        values = eliminate(values)
+        values = only_choice(values)
+        solved_values_after = len([box for box in values.keys() if len(values[box]) == 1])
+        stalled = solved_values_before == solved_values_after
+        if len([box for box in values.keys() if len(values[box]) == 0]):
+            return False
+    return values
 
 def search(values):
-    pass
+    """
+    NOTE: Code extracted from Udacity's Sudoku project lessons.
+
+    Using depth-first search and propagation, try all possible values.
+    """
+    # First, reduce the puzzle using the previous function
+    values = reduce_puzzle(values)
+    if values is False:
+        return False ## Failed earlier
+    if all(len(values[s]) == 1 for s in boxes):
+        return values ## Solved!
+    # Choose one of the unfilled squares with the fewest possibilities
+    n,s = min((len(values[s]), s) for s in boxes if len(values[s]) > 1)
+    # Now use recurrence to solve each one of the resulting sudokus, and
+    for value in values[s]:
+        new_sudoku = values.copy()
+        new_sudoku[s] = value
+        attempt = search(new_sudoku)
+        if attempt:
+            return attempt
 
 def solve(grid):
     """
